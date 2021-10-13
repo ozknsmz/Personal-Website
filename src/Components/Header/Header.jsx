b25e0bfd5e853bdd26bd1f6d8a96bb74f7be6a1c
ozknsmz <ozknsmz@hotmail.com>
2021-10-13 18:11:09 +0300

    Header page fixed

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,30 +1,41 @@
-import "./header.scss"
-import { init } from 'ityped'
-import { useEffect, useRef } from "react"
+import "./header.scss";
+import { init } from "ityped";
+import { useEffect, useRef } from "react";
 
 export default function Header() {
-    const textRef = useRef();
-    useEffect(() => {
-        init(textRef.current, {
-            showCursor: false,
-            backDelay: 1500,
-            backSpeed: 50,
-            strings: ["Developer", "Designer"],
-        })
-    }, []);
+  const textRef = useRef();
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: false,
+      backDelay: 1500,
+      backSpeed: 50,
+      strings: ["Developer", "Designer"],
+    });
+  }, []);
 
-    return (
-        <div className="Header" id="header">    
-                <div className="headerSide">
-                <div className="container">
-                    <div className="row">
-                        <div className="col">
-                            <h2>Hi There, I'm &nbsp;<span>Özkan Semiz</span></h2>
-                            <h3>A web <span ref={textRef}></span></h3>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <div className="Header-wraper" id="header">
+      <div className="main-info">
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <h2>
+              </h2>
+              <h3>
+                A web <span ref={textRef}></span>
+              </h3>
             </div>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
+}
+
+{
+  /* <img src={imgbg} alt="" />
+      <div className="headerSide">
+
+        </div>
+      </div> */
 }
